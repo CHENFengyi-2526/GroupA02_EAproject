dfa96ac5bdf86041e6416c851cf595d7786488bf
CHENFengyi-2526 <250384197@stu.vtc.edu.hk>
2026-04-15 02:51:20 +0000

    cookie

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -1,4 +1,4 @@
-from flask import render_template,Blueprint
+from flask import render_template,Blueprint,make_response, request, url_for, flash, redirect
 from app.models.tutorial import Tutorial
 from app.models.resource import Resource
 from flask_login import login_required
@@ -17,3 +17,19 @@ def user_profile(username):
     from app.models.user import User
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html.j2', user=user)
+
+@bp.route('/set-language/<lang>')
+def set_language(lang):
+    if lang not in ['en', 'zh', 'ja', 'ko']:
+        flash('Invalid language', 'danger')
+        return redirect(request.referrer or url_for('main.index'))
+    
+    resp = make_response(redirect(request.referrer or url_for('main.index')))
+    resp.set_cookie('preferred_language', lang, max_age=30*24*3600) 
+    flash(f'Language preference set to {lang}', 'success')
+    return resp
+
+@bp.route('/show-cookie')
+def show_cookie():
+    lang = request.cookies.get('preferred_language', 'not set')
+    return f'Your preferred language is: {lang}'
